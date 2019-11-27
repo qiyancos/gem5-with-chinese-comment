84614ebf93625d936964c78dad197710bfc8d5cf
qiyancos <lsk_mprc@pku.edu.cn>
2019-11-27 16:47:22 +0800

    remove some useless files

--- fullsystem.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-build/X86/gem5.opt configs/example/fs.py --cpu-type=DerivO3CPU --caches --l1d_size=64kB --l1d_assoc=8 --l1i_size=32kB --l1i_assoc=4 --mem-size=2GB -n 2 --kernel=x86_64-vmlinux-2.6.22.9.smp --disk-image=linux-x86.img
