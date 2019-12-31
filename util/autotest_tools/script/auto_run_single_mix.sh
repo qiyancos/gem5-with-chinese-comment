9a117bf0c53c380c050c9f906ffc86ca35f0c799
qiyancos <lsk_mprc@pku.edu.cn>
2019-12-31 16:18:34 +0800

    Add auto test script

---
+++ util/autotest_tools/script/auto_run_single_mix.sh
@@ -0,0 +1,170 @@
+#! /bin/bash
+set -e
+root=`dirname $0`
+cd $root/..
+root=$PWD
+
+cpuNum=`lscpu | awk '/^CPU\(s\):/{print $2}'`
+cpuNum=$[cpuNum * 3 / 5]
+testList=`$root/script/se -l`
+# testList=`$root/script/se -l | sed 's/\./ /g' | awk '{print $1}'`
+# testFile=`ls $root/test_script`
+testFile="core8_l1np_l2np  core8_l1p2_l2np  core8_l1p4_l2np core8_l1p8_l2np"
+testTaskNums="8"
+newTaskGap="1m"
+
+##############################################################################
+
+runTask() {
+    echo "-- Start running test \"$file\" for $testName with $taskNum task(s):"
+    taskDir=$root/data/pre_test/$file/$testName/$taskNum
+    mkdir -p $taskDir
+    cd $root/data/pre_test/$file/$testName/$taskNum
+    rm -rf $taskDir/spec
+    testNum=`echo $testName | sed 's/\..*//g'`
+    unset realTestName realTestDir
+    for((i = 0; i < $taskNum; i++))
+    do
+        realTestDir="${realTestDir}_${testNum}"
+        realTestName="${realTestName} ${testNum}"
+    done
+    realTestDir=${realTestDir:1:$[${#realTestDir} - 1]}
+    echo "-- Running Command: $root/test_script/$file -p $realTestName"
+    set +e
+    $root/test_script/$file -p $realTestName
+    set -e
+    if [ -s $taskDir/spec/$realTestDir/m5out/stats* ]
+    then
+        cp -r $taskDir/spec/$realTestDir/m5out/* $taskDir/
+        echo "-- \"$file\" test over for $testName with $taskNum task(s)."
+    else
+        echo -n "Error: some errors occurred with test: "
+        echo "$file-$testName-$taskNum"
+        echo $task >> /tmp/retry.list
+    fi
+    rm -rf $taskDir/spec
+}
+
+initRunTask() {
+    echo "-- Generating task list..."
+    for file in $testFile
+    do
+        fileName=(`echo $file | sed 's/_/ /g'`)
+        for taskNum in $testTaskNums
+        do
+            for testName in $testList
+            do runTasks="$runTasks ${file}:${taskNum}:${testName}"
+            done
+        done
+    done
+    
+    if [ -f /tmp/mp.lock ]
+    then
+        echo "Error: Multi program is already locked."
+        exit -1
+    else
+        lockID=`date | base64 -i`
+        echo "$lockID" > /tmp/mp.lock
+        echo "0 idle" > /tmp/mp.list
+        threadID=1
+        while [ $threadID -lt $cpuNum ]
+        do
+            echo "$threadID idle" >> /tmp/mp.list
+            threadID=$[threadID + 1]
+        done
+        rm /tmp/mp.lock
+    fi
+}
+
+atomicChange() {
+    lockID=`date | base64 -i`
+    echo ">> Trying get lock..."
+    while [ -f /tmp/mp.lock ]
+    do sleep 0.1
+    done
+    while [ ! -f /tmp/mp.lock ]
+    do echo "$lockID" > /tmp/mp.lock
+    done
+    
+    while [ x"`cat /tmp/mp.lock`" != x$lockID ]
+    do
+        while [ -f /tmp/mp.lock ]
+        do sleep 0.1
+        done
+        while [ ! -f /tmp/mp.lock ]
+        do echo "$lockID" > /tmp/mp.lock
+        done
+    done
+    echo ">> Control file locked."
+    sed -i "/^$1 /c$1 $2" /tmp/mp.list
+    rm /tmp/mp.lock
+    echo ">> Control file unlocked."
+}
+
+runAllTask() {
+    for task in $runTasks
+    do
+        echo "-- Trying to find idle cpu for new task($task)..."
+        unset idleThreadID
+        unset nocpuInfo
+        while [ 1 ]
+        do
+            threadID=0
+            while [ $threadID -lt $cpuNum ]
+            do
+                if [ "x`grep "^$threadID idle" /tmp/mp.list`" != x ]
+                then
+                    idleThreadID=$threadID
+                    break
+                fi
+                threadID=$[threadID + 1]
+            done
+            if [ x$idleThreadID = x ]
+            then
+                if [ x$nocpuInfo = x ]
+                then
+                    echo "-- No cpu available now."
+                    nocpuInfo=1
+                fi
+                sleep 20
+            else
+                echo "-- Found cpu $idleThreadID available for task($task)."
+                break
+            fi
+        done
+        atomicChange $idleThreadID "run $task"
+        taskArr=(`echo $task | sed 's/:/ /g'`)
+        file=${taskArr[0]}
+        taskNum=${taskArr[1]}
+        testName=${taskArr[2]}
+        {
+            runTask
+            atomicChange $idleThreadID "idle"
+        } &
+        sleep $newTaskGap
+    done
+}
+
+retryRunTask() {
+    retryTasks=$(cat /tmp/retry.list)
+    if [ "x$retryTasks" != x ]
+    then
+        echo "-- Following task didn't run properly:"
+        for task in $retryTasks
+        do echo -e "\t$task"
+        done
+        runTasks=$retryTasks
+        retryFlag=1
+    else retryFlag=0
+    fi
+}
+
+initRunTask
+while [ x$retryFlag != x -o x$retryFlag = x1 ]
+do
+    rm -rf /tmp/retry.list
+    echo "-- Start running left tasks."
+    runAllTask
+    echo "-- Left tasks running over."
+    retryRunTask
+done
---
+++ util/autotest_tools/script/auto_stats.sh
@@ -0,0 +1,142 @@
+#! /bin/bash
+localDir=$PWD
+root=`dirname $0`
+cd $root/..
+root=$PWD
+
+cpuNum=`lscpu | awk '/^CPU\(s\):/{print $2}'`
+testList=`$root/script/se -l`
+# testFile=`ls $root/test_script`
+eval "$(grep "testFile=" $root/script/auto_run_single_mix.sh)"
+testTaskNums="8"
+statsTitle="TestName, Task Number, Test Subset"
+
+singleStatsList=("")
+singleStatsString=("")
+multiStatsList=("IPC"
+        "L1D_Pref_Issued" "L1D_Pref_Identidied" "L1D_Pref_BufferHit"
+        "L1D_Pref_CacheHit" "L1D_Pref_RemoveFull" "L1D_Pref_RemoveCrossPage"
+        "L2_Pref_Issued" "L2_Pref_Identidied" "L2_Pref_BufferHit"
+        "L2_Pref_CacheHit" "L2_Pref_RemoveFull" "L2_Pref_RemoveCrossPage")
+multiStatsString=("ipc_total"
+        "dcache.prefetcher.num_hwpf_issued" "dcache.prefetcher.pfIdentified"
+        "dcache.prefetcher.pfBufferHit" "dcache.prefetcher.pfInCache"
+        "dcache.prefetcher.pfRemovedFull" "dcache.prefetcher.pfSpanPage"
+        "l2.prefetcher.num_hwpf_issued" "l2.prefetcher.pfIdentified"
+        "l2.prefetcher.pfBufferHit" "l2.prefetcher.pfInCache"
+        "l2.prefetcher.pfRemovedFull" "l2.prefetcher.pfSpanPage")
+
+statsList="IPC"
+
+################################################################
+
+singleStats() {
+    statsIndex=0
+    while [ x${singleStatsList[$statsIndex]} != x ]
+    do
+        if [ ${singleStatsList[$statsIndex]} = $1 ]
+        then break
+        else statsIndex=$[statsIndex + 1]
+        fi
+    done
+    if [ $statsIndex = ${#singleStatsList[*]} ]
+    then
+        echo "Error: Stats \"$1\" not found in single stats list."
+        exit -1
+    fi
+    statsString=${singleStatsString[$statsIndex]}
+    statsVal=`eval "awk '/$statsString/ {print \\$2}' $2"`
+    if [ x$(eval "echo \$statsFlag$1") = x ]
+    then
+        statsTitle="$statsTitle, $1"
+        eval "statsFlag$1=1"
+    fi
+    echo -n "$statsVal, "
+}
+
+multiStats() {
+    statsIndex=0
+    while [ x${multiStatsList[$statsIndex]} != x ]
+    do
+        if [ ${multiStatsList[$statsIndex]} = $1 ]
+        then break
+        else statsIndex=$[statsIndex + 1]
+        fi
+    done
+    if [ $statsIndex = ${#multiStatsList[*]} ]
+    then
+        echo "Error: Stats \"$1\" not found in multi stats list."
+        exit -1
+    fi
+    statsString=${multiStatsString[$statsIndex]}
+    statsVal=(`eval "awk '/$statsString/ {print \\$2}' $2"`)
+    index=0
+    if [ x$(eval "echo \$statsFlag$1") = x ]
+    then
+        eval "statsFlag$1=1"
+        index=0
+        while [ $index -lt $coreNum ]
+        do
+            statsTitle="$statsTitle, ${1}_$index"
+            echo -n "${statsVal[$index]}, "
+            index=$[index + 1]
+        done
+    else
+        index=0
+        while [ $index -lt $coreNum ]
+        do
+            echo -n "${statsVal[$index]}, "
+            index=$[index + 1]
+        done
+    fi
+}
+
+statsFile() {
+    taskDir=$root/data/pre_test/$file/$testName/$taskNum
+    fileName="$taskDir/stats*"
+    if [ -f $fileName ]
+    then
+        echo -n "$file, $taskNum, $testName, " >> $1
+        for stats in $statsList
+        do
+            if [ "x`echo ${multiStatsList[*]} | grep $stats`" != x ]
+            then multiStats $stats $fileName >> $1
+            elif [ "x`echo ${singleStatsList[*]} | grep $stats`" != x ]
+            then singleStats $stats $fileName >> $1
+            else
+                echo "Error: Stats \"$stats\" not found in statsList."
+                exit -1
+            fi
+        done
+        echo >> $1
+    else echo "$file, $taskNum, $testName, Error" >> $1
+    fi
+}
+
+if [ x$1 = x ]
+then
+    echo ">> Result will be saved to $localDir/stats.csv"
+    targetFile=$localDir/stats.csv
+else targetFile=$1
+fi
+rm -rf $targetFile
+mkdir -p `dirname $targetFile`
+if [ $? != 0 ]
+then
+    echo "Error: Failed to build dir for target \"$targetFile\"."
+    exit -1
+fi
+
+for file in $testFile
+do
+    coreNum=`echo $file | sed 's/core\([0-9]*\).*/\1/g'`
+    echo "-- Processing Test File $file"
+    for testName in $testList
+    do
+        for taskNum in $testTaskNums
+        do statsFile $targetFile
+        done
+        echo "   $testName Done."
+    done
+done
+sed -i "1i\\$statsTitle" $targetFile
